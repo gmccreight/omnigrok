ec2c64d1dc9c520319fcbaa084934e58329836a9
Gordon McCreight <gordon@mccreight.com>
2011-06-19 15:45:05 -0700

    Add a standalone compile script

---
+++ _app/compile.rb
@@ -0,0 +1,183 @@
+#!/usr/bin/env ruby
+
+$commands = []
+$results = ""
+$is_dry_run = true
+$run_practice = false
+
+$debug = true
+
+def main
+
+  mode = ARGV[0]
+
+  if !mode
+    die "Please specify a mode as your first argument"
+  end
+
+  if mode == "run_normal" || mode == "run_practice" || mode == "dry_run" || mode == "do_all_tests_pass"
+    dir = ARGV[1] + ""
+    dir.gsub!(/\/$/,"") #trim trailing slash if it exists
+    if !dir
+      die "Please specify a directory as your second argument"
+    end
+    if mode == "run_normal" || mode == "run_practice" || mode == "do_all_tests_pass"
+      $is_dry_run = false
+    end
+    if mode == "run_practice"
+      $run_practice = true
+    end
+    run_tests_for_dir(dir)
+    if mode == "run_normal" || mode == "run_practice"
+      puts $results
+    elsif mode == "do_all_tests_pass"
+      h = info_for_type(type_for_dir(dir))
+      if $results =~ %r{#{h[:passing_regex_str]}}
+        puts "ALL TESTS PASSED"
+      else
+        puts $results
+        puts "TESTS DID NOT ALL PASS"
+      end
+    end
+  else
+    die "unrecognized mode #{mode}"
+  end
+end
+
+def die(message)
+  STDERR.puts "Whoops: #{message}"
+  exit
+end
+
+def info_for_type(type)
+
+  suffix = code_suffix_for_type(type)
+  if !suffix
+    STDERR.puts "Could not get the suffix for the type #{type}"
+    return
+  end
+
+  if $run_practice
+    sourcecode = "practice." + suffix
+  else
+    sourcecode = "code." + suffix
+  end
+
+  tee_results = "| tee ../../_app/tests/integration/tmp_tests_out.txt"
+
+  h = {:commands => [], :passing_regex_str => nil}
+
+  if type == "c"
+    # It's a C directory
+    # Using Check, a unit testing framework for C
+    h[:commands] << "gcc -o code.o -c #{sourcecode}"
+    h[:commands] << "gcc -o unittests.o -c unittests.c"
+    h[:commands] << "gcc -o unittests ../../_app/tests/frameworks/c_check/src/*.o code.o unittests.o"
+    h[:commands] << "./unittests #{tee_results}"
+    h[:commands] << "rm ./unittests"
+    h[:commands] << "rm *.o"
+
+    h[:passing_regex_str] = "^100%"
+  elsif type == "cc"
+    # It's a C++ directory
+    # Using Googletest, Google's C++ testing framework, version 1.4.0
+    h[:commands] << "g++ -o code.o -c #{sourcecode}"
+    h[:commands] << "g++ $(gtest-config --cppflags --cxxflags) -o unittests.o -c unittests.cc"
+    h[:commands] << "g++ $(gtest-config --ldflags --libs) -o unittests ../../_app/tests/frameworks/cc_gtest/gtest_main.o code.o unittests.o"
+    h[:commands] << "./unittests #{tee_results}"
+
+    h[:passing_regex_str] = "PASSED"
+  elsif type == "coffee"
+    # It's a coffeescript directory
+    # Uses Rhino
+    h[:commands] << "coffee -b -c *.coffee"
+    #special case for coffeescript
+    if $run_practice
+      h[:commands] << "cp practice.js code_or_practice_copied.js"
+    else
+      h[:commands] << "cp code.js code_or_practice_copied.js"
+    end
+    h[:commands] << "java -jar ../../_app/tests/frameworks/js/js.jar unittests.js #{tee_results}"
+    h[:commands] << "rm code.js practice.js unittests.js" #special case for coffeescript
+
+    h[:passing_regex_str] = "ALL TESTS PASSED"
+  elsif type == "js"
+    # It's a Javascript directory
+    # Uses Rhino
+    h[:commands] << "cp #{sourcecode} code_or_practice_copied.js"
+    h[:commands] << "java -jar ../../_app/tests/frameworks/js/js.jar unittests.js | tee ../../_app/tests/integration/tmp_tests_out.txt"
+
+    h[:passing_regex_str] = "ALL TESTS PASSED"
+  elsif type == "objc"
+    # It's an objective c directory
+    h[:commands] << "gcc -c -Wno-import #{sourcecode}"
+    h[:commands] << "gcc -c -Wno-import unittests.m"
+    h[:commands] << "gcc -o unittests -Wno-import #{sourcecode} unittests.o -lobjc"
+    h[:commands] << "./unittests #{tee_results}"
+
+    h[:passing_regex_str] = "TODO - We don't have a unit test framework yet"
+  elsif type == "rb"
+    # It's a ruby directory
+    # Using test unit
+    h[:commands] << "cp #{sourcecode} code_or_practice_copied.rb"
+    h[:commands] << "ruby unittests.rb #{tee_results}"
+  elsif type == "scala"
+    # It's a Scala directory
+    shared = "PATH=$PATH:#{Dir.getwd}/_app/local/scala/bin;"
+    h[:commands] << "#{shared} scalac -cp ../../_app/tests/frameworks/scalatest/scalatest-1.6.1.jar code.scala unittests.scala"
+    h[:commands] << "#{shared} scala -cp ../../_app/tests/frameworks/scalatest/scalatest-1.6.1.jar org.scalatest.tools.Runner -p . -o -s unittests #{tee_results}"
+
+    h[:passing_regex_str] = "All tests passed"
+  end
+
+  return h
+end
+
+# foo/blah/stack_scala is type scala
+# foo/bar/tree_coffee is type coffee
+def type_for_dir(dir)
+  if type = dir.match(/.+_([a-z]+)$/)
+    return type[1]
+  end
+  nil
+end
+
+def code_suffix_for_type(type)
+  return nil if type == nil
+  if type == "objc"
+    return "m"
+  else
+    return type
+  end
+end
+
+def cleanup
+  $commands << "rm -f *.o"
+  $commands << "rm -f *.class"
+  $commands << "rm -f *.unittests"
+  $commands << "rm -rf code"
+  $commands << "rm -f code_or_practice_copied.*"
+end
+
+def _run_commands
+  $commands.each do |c|
+    if $is_dry_run
+      puts c
+    else
+      $results << `#{c}`
+    end
+  end
+end
+
+def run_tests_for_dir(dir)
+  cleanup()
+  h = info_for_type(type_for_dir(dir))
+  h[:commands].each do |c|
+    $commands << c
+  end
+  cleanup()
+  Dir.chdir dir
+  _run_commands()
+end
+
+main()
--- _app/rosetta_cs.vim
+++ _app/rosetta_cs.vim
@@ -49,88 +49,14 @@ map ,p :call BufferOrEdit(expand("%:h") . "/practice")<cr>
 
 map <c-j> :call RunUnitTestsForDir()<cr>
 
-" If directories have a standard structure, it's easy to run the unit tests
-" in them.  See the 'linked_list' directory for an example.
+" Directories have a standard structure, it's easy to run the unit tests.
 function! RunUnitTestsForDir()
-    let cwd = getcwd()
+    write
     let dir = expand("%:h")
     let current_file = expand("%")
-
-    execute "cd " . dir
-    
-    " If you happen to be editing the practice file, then compile using the
-    " practice file instead of the standard "code" file
-    let sourcecode = match(current_file, "practice") > 0 ? "practice" : "code" 
-
-    if match(dir, "_c$") > 0
-        " It's a C directory
-        " Using Check, a unit testing framework for C
-        write
-        silent !rm *.o
-        exec "!gcc -o code.o -c " . sourcecode . ".c"
-        !gcc -o unittests.o -c unittests.c
-        silent !rm ./unittests
-        !gcc -o unittests ../../_app/tests/frameworks/c_check/src/*.o code.o unittests.o
-        !./unittests | tee ../../_app/tests/integration/tmp_tests_out.txt
-        silent !rm ./unittests
-        silent !rm *.o
-    elseif match(dir, "_cc$") > 0
-        " It's a C++ directory
-        " Using Googletest, Google's C++ testing framework, version 1.4.0
-        write
-        silent !rm *.o
-        exec "!g++ -o code.o -c " . sourcecode . ".cc"
-        !g++ $(gtest-config --cppflags --cxxflags) -o unittests.o -c unittests.cc
-        silent !rm ./unittests
-        !g++ $(gtest-config --ldflags --libs) -o unittests ../../_app/tests/frameworks/cc_gtest/gtest_main.o code.o unittests.o
-        !./unittests | tee ../../_app/tests/integration/tmp_tests_out.txt
-        silent !rm ./unittests
-        silent !rm *.o
-    elseif match(dir, "_coffee$") > 0
-        " It's a coffeescript directory
-        " Uses Rhino
-        write
-        exec "!coffee -b -c *.coffee"
-        exec "!cp " . sourcecode . ".js code_or_practice_copied.js"
-        !java -jar ../../_app/tests/frameworks/js/js.jar unittests.js | tee ../../_app/tests/integration/tmp_tests_out.txt
-        silent !rm code.js
-        silent !rm practice.js
-        silent !rm code_or_practice_copied.js
-        silent !rm unittests.js
-    elseif match(dir, "_objc$") > 0
-        " It's an objective c directory
-        write
-        silent !rm *.o
-        exec "!gcc -c -Wno-import " . sourcecode . ".m"
-        !gcc -c -Wno-import unittests.m
-        exec "!gcc -o unittests -Wno-import " . sourcecode . ".o unittests.o -lobjc"
-        !./unittests | tee ../../_app/tests/integration/tmp_tests_out.txt
-        silent !rm ./unittests
-        silent !rm *.o
-    elseif match(dir, "_rb$") > 0
-        " It's a ruby directory
-        " Using test unit
-        write
-        exec "!cp " . sourcecode . ".rb code_or_practice_copied.rb" 
-        !ruby unittests.rb | tee ../../_app/tests/integration/tmp_tests_out.txt
-        silent !rm code_or_practice_copied.rb 
-    elseif match(dir, "_js$") > 0
-        " It's a Javascript directory
-        " Uses Rhino
-        write
-        exec "!cp " . sourcecode . ".js code_or_practice_copied.js" 
-        !java -jar ../../_app/tests/frameworks/js/js.jar unittests.js | tee ../../_app/tests/integration/tmp_tests_out.txt
-        silent !rm code_or_practice_copied.js
-    elseif match(dir, "_scala$") > 0
-        " It's a Scala directory
-        write
-        silent !rm -rf code
-        silent !rm ./*.class
-        exec "!PATH=$PATH:" . cwd . "/_app/local/scala/bin; scalac -cp ../../_app/tests/frameworks/scalatest/scalatest-1.6.1.jar code.scala unittests.scala"
-        exec "!PATH=$PATH:" . cwd . "/_app/local/scala/bin; scala -cp ../../_app/tests/frameworks/scalatest/scalatest-1.6.1.jar org.scalatest.tools.Runner -p . -o -s unittests"
-        silent !rm ./*.class
-        silent !rm -rf code
+    if match(current_file, "practice") > 0
+      exec "!./_app/compile.rb run_practice " . dir
+    else
+      exec "!./_app/compile.rb run_normal " . dir
     endif
-
-    execute "cd " . cwd
 endfunction
