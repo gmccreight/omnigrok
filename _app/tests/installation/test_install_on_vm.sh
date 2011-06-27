d6fbd7d5e15cc91b20f0e56c64986e9b523e8e59
Gordon McCreight <gordon@mccreight.com>
2011-06-27 10:57:47 -0700

    Added two options to test_install_on_vm script

--- _app/tests/installation/test_install_on_vm.sh
+++ _app/tests/installation/test_install_on_vm.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
-./provisionator.sh rosetta_cs 1-50
+mode=$1
+
+if [ `echo $mode | egrep "^(all|copy_install_and_test)$" | wc -l`  -eq 0 ] ; then
+  echo "error: not a good mode.  Must be 'all' or 'copy_install_and_test'"
+  exit
+fi
+
+if [ `echo $mode | egrep "^all$" | wc -l`  -eq 0 ] ; then
+  ./provisionator.sh rosetta_cs 0-50
+fi
+
+if [ `echo $mode | egrep "^copy_install_and_test$" | wc -l`  -eq 0 ] ; then
+  ./provisionator.sh rosetta_cs 11,30-50
+fi
