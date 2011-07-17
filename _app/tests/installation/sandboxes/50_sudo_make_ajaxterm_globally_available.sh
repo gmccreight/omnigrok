ec7fd1d37d52a1858555377dc5f2f5cd2d444141
Gordon McCreight <gordon@mccreight.com>
2011-07-16 22:19:28 -0700

    Make the terminal bigger in the browser window

--- _app/tests/installation/sandboxes/50_sudo_make_ajaxterm_globally_available.sh
+++ _app/tests/installation/sandboxes/50_sudo_make_ajaxterm_globally_available.sh
@@ -60,3 +60,17 @@ ENDOFFILE
 # Restart Apache
 
 /etc/init.d/apache2 restart
+
+#----------------------------------------------------------------------------------
+# Make the terminal the ajaxterm shows fill up more of the screen as described here:
+# http://wiki.kartbuilding.net/index.php/Ajaxterm#Larger_Terminal_Window
+
+sed -i 's:ajaxterm.Terminal.*:ajaxterm.Terminal("term", 160, 40);:' /usr/share/ajaxterm/ajaxterm.html
+
+#override the width and height passed in to the method by explicitly adding them in two lines following the __init__
+sed -i '/__init__.*width/{n;s/^/\t\twidth = 160\n\t\theight = 40\n/}' /usr/share/ajaxterm/ajaxterm.py
+
+/etc/init.d/ajaxterm restart
+
+# End making terminal bigger
+#----------------------------------------------------------------------------------
