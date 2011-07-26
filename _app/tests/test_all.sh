2b723da6e405aeef755f699dc60a090ee7c1a0ae
Gordon McCreight <gordon@mccreight.com>
2011-07-26 16:54:19 -0700

    Moved playground to unfinished

--- _app/tests/test_all.sh
+++ _app/tests/test_all.sh
@@ -3,7 +3,7 @@
 #Run all the directories and see if there are any issues
 
 cd ../../
-find . -maxdepth 1 \( -name .git -o -name _app -o -name playground \) -prune -o -type d -print | while read dir
+find . -maxdepth 1 \( -name .git -o -name _app -o -name unfinished \) -prune -o -type d -print | while read dir
 do
   if [ $dir != "." ] ; then
     find $dir -mindepth 1 -maxdepth 1 -type d | while read fulldir
