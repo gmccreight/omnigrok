474cd1290678734c329eb36d90782826fae09efa
Gordon McCreight <gordon@mccreight.com>
2011-07-26 18:26:12 -0700

    Missed a couple files during rename

--- _app/tests/installation/test_install_on_vm.sh
+++ _app/tests/installation/test_install_on_vm.sh
@@ -8,13 +8,13 @@ if [ `echo $mode | egrep "^(all|run_copy_install_and_test|copy_install_and_test)
 fi
 
 if [ `echo $mode | egrep "^all$" | wc -l`  -eq 1 ] ; then
-  ./provisionator.sh omnigrok 0-50
+  ./provisionator.sh og 0-50
 fi
 
 if [ `echo $mode | egrep "^run_copy_install_and_test$" | wc -l`  -eq 1 ] ; then
-  ./provisionator.sh omnigrok 11,30-50
+  ./provisionator.sh og 11,30-50
 fi
 
 if [ `echo $mode | egrep "^copy_install_and_test$" | wc -l`  -eq 1 ] ; then
-  ./provisionator.sh omnigrok 30-50
+  ./provisionator.sh og 30-50
 fi
