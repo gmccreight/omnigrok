ca6128fac3523d80bf2f9e8b4e6794fdd514a27d
Gordon McCreight <gordon@mccreight.com>
2011-07-27 23:03:22 -0700

    Add coffeescript syntax

---
+++ _app/sandboxes/install/og/64_install_vim_nerdtree.sh
@@ -0,0 +1,8 @@
+sudo apt-get install unzip -y
+
+cd ~
+mkdir .vim
+cd .vim
+wget -O n.zip http://www.vim.org/scripts/download_script.php?src_id=11834
+unzip n.zip
+rm n.zip
---
+++ _app/sandboxes/install/og/66_install_vim_coffeescript.sh
@@ -0,0 +1,8 @@
+sudo apt-get install unzip -y
+
+cd ~
+mkdir .vim
+cd .vim
+wget -O c.zip http://www.vim.org/scripts/download_script.php?src_id=16124
+unzip c.zip
+rm c.zip
