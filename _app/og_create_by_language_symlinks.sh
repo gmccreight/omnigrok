d36bd32350bb50c6e93c325d7c0e883861621900
Gordon McCreight <gordon@mccreight.com>
2011-09-17 11:18:54 -0700

    renamed create_languages script and folder

---
+++ _app/og_create_by_language_symlinks.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#create and populate the by_language folder with symlinks to the actual working code
+
+rm -r by_language
+
+find . -maxdepth 1 \( -name .git -o -name _app -o -name unfinished -o -name by_language \) -prune -o -type d -print | while read dir
+do
+  if [ $dir != "." ] ; then
+    find $dir -mindepth 2 -maxdepth 2 -type d | while read fulldir
+    do
+      lang=$(echo $fulldir | egrep -o "_[a-z]+$" | egrep -o "[a-z]+")
+      mkdir -p by_language/$lang
+      ln -s ../../$fulldir by_language/$lang/$(basename $fulldir)
+    done
+  fi
+done
